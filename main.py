a1e878c221bc3217be42b4105d47527c1dc4cff3
Ishan Raj Sapkota <ishansapkota2@gmail.com>
2022-07-02 22:28:37 +0545

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#  WAP to display the poem "Twinkle twinkle little star"
+# print('''Twinkle twinkle little star 
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky''')
+
+#WAP to print the contents of directory using OS module.
+
+import os
+
+print(os.listdir())
